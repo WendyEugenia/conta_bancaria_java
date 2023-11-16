cb36e383783d6b1b114750d6ac8d955ae7e644b4
WendyEugenia <wendy.t.n@hotmail.com>
2023-11-16 12:33:48 -0300

    Menu do Projeto

---
+++ src/conta/Menu.java
@@ -0,0 +1,13 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+	}
+
+}
